91bf9f80f689432a99ce727982e69e39518f1cc3
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-01 16:41:19 -0800

    created food dropdown

--- src/Dropdowns/FoodDropdown.js
+++ src/Dropdowns/FoodDropdown.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function FoodDropdown() {
-  return <div></div>;
+export default function FoodDropdown({ setFoodId }) {
+  return <select onChange={(e) => {setFoodId(e.target.value); }} >
+    <option value='1'>Hamburger</option>
+    <option value='2'>Fish n Chips</option>
+    <option value='3'>Salad</option>
+
+  </select>;
 }
