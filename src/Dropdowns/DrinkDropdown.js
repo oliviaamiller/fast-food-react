cd4391497773f38c4f23d3036e82e64b16f92e3c
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-01 16:38:26 -0800

    created drink dropdown

--- src/Dropdowns/DrinkDropdown.js
+++ src/Dropdowns/DrinkDropdown.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function DrinkDropdown() {
-  return <div></div>;
+export default function DrinkDropdown({ setDrinkId }) {
+  return <select onChange={(e) => {setDrinkId(e.target.value);}} >
+    <option value='1'>Milkshake</option>
+    <option value='2'>Limeade</option>
+    <option value='3'>Coffee</option>
+
+  </select>;
 }
